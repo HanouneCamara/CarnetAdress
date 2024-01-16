4d0cf908d7d95805c6e3263ad41c0f5a48a64619
HanouneCamara <Alicmr13@gmail.com>
2024-01-16 18:07:09 +0000

    addPandasAndCsv

--- main.py
+++ main.py
@@ -1,24 +1,46 @@
-contacts = {}
+import pandas as pd
 
-def Ajouter_contact():
-    nom = input("Nom : ")
-    prenom = input("Prénom : ")
-    adresse = input("Adresse : ")
-    numero_tel = input("Numéro de téléphone : ")
-    contacts[nom] = {
-        'prenom': prenom,
-        'adresse': adresse,
-        'numero_tel': numero_tel
-    }
-    print('Contact ajouté!')
-    
-def Afficher_contact():
-    for nom, infos in contacts.items():
-        print(f"Nom :  {nom}")
-        print(f"Prénom : {infos['prenom']}")
-        print(f"Adresse : {infos['adresse']}")
-        print(f"Numéro de téléphone : {infos['numero_tel']}")
-        
-Ajouter_contact()
-print("Liste des contacts : ")
-Afficher_contact()
+csv_filename = "donnees_utilisateur.csv"
+
+def saisir_donnees():
+    # Demander à l'utilisateur de saisir les données
+    name_input = input("Entrez le nom (séparé par des virgules si plusieurs noms) : ")
+    lastName_input = input("Entrez le prenom (séparé par des virgules si plusieurs prénoms) : ")
+    adress_input = input("Entrez l'adresse (séparé par des virgules si plusieurs adresses) : ")
+    number_input = input("Entrez le numéro de téléphone (séparé par des virgules si plusieurs numéros) : ")
+
+    # Convertir les entrées en listes
+    names = [name.strip() for name in name_input.split(',')]
+    lastNames = [lastNames.strip() for lastNames in lastName_input.split(',')]
+    adress = [adress.strip() for adress in adress_input.split(',')]
+    num = [num.strip() for num in number_input.split(',')]
+
+    # Créer le DataFrame
+    df = pd.DataFrame({"Names": names,"Last Name": lastNames, "Adresses": adress, "Number phone": num})
+
+    # Ajouter les nouvelles données au fichier CSV existant
+    try:
+        existing_df = pd.read_csv(csv_filename)
+        df = pd.concat([existing_df, df], ignore_index=True)
+    except FileNotFoundError:
+        pass  # Le fichier n'existe pas encore, il sera créé lors de la sauvegarde ci-dessous
+
+    # Stocker les données dans le fichier CSV
+    df.to_csv(csv_filename, index=False)
+
+    print(f"Les données ont été enregistrées dans le fichier CSV : {csv_filename}")
+
+def afficher_donnees():
+    try:
+        # Afficher les données depuis le fichier CSV
+        df_from_csv = pd.read_csv(csv_filename)
+        print("\nAffichage des données depuis le fichier CSV :")
+        print(df_from_csv)
+    except FileNotFoundError:
+        print(f"Le fichier CSV '{csv_filename}' ne contient aucune donnée.")
+
+# Exécuter la fonction pour saisir les données
+saisir_donnees()
+
+# Exécuter la fonction pour afficher les données
+afficher_donnees()
